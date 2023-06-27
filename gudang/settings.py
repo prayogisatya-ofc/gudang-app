0767dfb7029ced77293cecc859339fa405555d44
Prayogi Setiawan <besutodio@gmail.com>
2023-06-27 09:24:54 +0700

    Update settings.py

--- gudang/settings.py
+++ gudang/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qk-(k7zbwr*2v0=-3adw558@xt$05j8ohzaq2@m)g!i%%(utp$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
